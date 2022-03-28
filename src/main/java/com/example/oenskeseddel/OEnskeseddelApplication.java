f08b4527b3134d22751ae85b074a6cb0fd254e72
m00ni <m00nice@live.com>
2022-03-28 09:01:41 +0200

    Initial commit

---
+++ src/main/java/com/example/oenskeseddel/OEnskeseddelApplication.java
@@ -0,0 +1,13 @@
+package com.example.oenskeseddel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OEnskeseddelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OEnskeseddelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oenskeseddel/OEnskeseddelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oenskeseddel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OEnskeseddelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
